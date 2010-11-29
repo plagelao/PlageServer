3310ea9e1634570e8c7113f1a5e17268f08743b4
Alberto <plagelao@gmail.com>
2010-11-29 16:51:16 +0000

    HttpMethod class is working for a GET!

--- spec/method.rb
+++ spec/method.rb
@@ -1,9 +1,12 @@
 require '../server/http_method.rb'
 
-describe "PlageUrl" do
+describe "Http method" do
 
   it "should be valid if it is a GET" do
     HttpMethod.new('GET').should be_valid
   end
 
+  it "should not be valid if it makes no sense" do
+    HttpMethod.new('blablabla').should_not be_valid
+  end
 end
