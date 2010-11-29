3b2f75bebc88ac0a73d09d7a21d62a39c89ddcd2
Alberto <plagelao@gmail.com>
2010-11-29 16:33:02 +0000

    Added the URL class

--- spec/request_line.rb
+++ spec/request_line.rb
@@ -1,9 +1,9 @@
-require './server/plage_server.rb'
+require '../server/plage_server.rb'
 
 describe "RequestLine" do
 
-  it "should be well formed (Starts with GET, is follow by an URI and the protocol version, ends with CRLF, separate every token with an empty space except the CRLF)" do
-    RequestLine.new("GET http://www.edendevelopment.es HTPP/1.1\u000D\u000A").should be_well_formed
+  it "should be well formed (Starts with a method, is follow by an URI and the protocol version, ends with CRLF, separate every token with an empty space except the CRLF)" do
+    RequestLine.new("GET http://www.edendevelopment.es HTTP/1.1\u000D\u000A").should be_well_formed
   end
 
   it "should not be well formed when it makes no sense" do
@@ -21,4 +21,13 @@ describe "RequestLine" do
   it "should not be well formed if second token is not a valid http URL" do
     RequestLine.new("GET notAnURI").should_not be_well_formed 
   end
+
+  it "should not be well formed if the third token is not a valid http version" do
+    RequestLine.new("GET http://www.edendevelopment.es not_a_valid_http_protocol_version").should_not be_well_formed
+  end
+
+  it "should not be well formed if the end of the string is not CRLF" do
+    RequestLine.new("GET http://www.edendevelopment.es HTTP/1.1").should_not be_well_formed
+  end
+
 end
---
+++ spec/spec_helper.rb
@@ -0,0 +1,4 @@
+require 'rspec'
+Dir.glob("*.rb") do |filename| 
+  require File.expand_path(filename)
+end
