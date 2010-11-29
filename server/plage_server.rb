9ac3399e2d475f92fb2477bc08dba9a369cbf240
Alberto <plagelao@gmail.com>
2010-11-29 14:07:03 +0000

    Finding out if the second token is a valid http url

--- server/plage_server.rb
+++ server/plage_server.rb
@@ -5,6 +5,10 @@ class RequestLine
   end
 
   def well_formed?
-    @request_line.split.at(0) == 'GET'
+    tokens = @request_line.split
+    if (tokens.at(0) != 'GET')
+      return false
+    end
+    tokens.at(1).start_with?("http://")
   end
 end
--- spec/request_line.rb
+++ spec/request_line.rb
@@ -3,8 +3,7 @@ require './server/plage_server.rb'
 describe "RequestLine" do
 
   it "should be well formed (Starts with GET, is follow by an URI and the protocol version, ends with CRLF, separate every token with an empty space except the CRLF)" do
-    get_request_line = "GET http://www.edendevelopment.es HTPP/1.1\u000D\u000A"
-    RequestLine.new(get_request_line).should be_well_formed
+    RequestLine.new("GET http://www.edendevelopment.es HTPP/1.1\u000D\u000A").should be_well_formed
   end
 
   it "should not be well formed when it makes no sense" do
@@ -15,7 +14,11 @@ describe "RequestLine" do
     RequestLine.new("GEThackhackhack").should_not be_well_formed
   end
 
-  it "should not be well formed if there first tiken is not a GET" do
+  it "should not be well formed if the first token is not a GET" do
     RequestLine.new("hackhackhack GET").should_not be_well_formed
+  end
+
+  it "should not be well formed if second token is not a valid http URL" do
+    RequestLine.new("GET notAnURI").should_not be_well_formed 
   end
 end
