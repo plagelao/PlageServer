38caf3e69bb264816c14699656b66e1d9dee43c4
Alberto <plagelao@gmail.com>
2010-11-30 14:53:28 +0000

    re-thinking the server

--- spec/request.rb
+++
@@ -1,9 +0,0 @@
-require '../server/http_request.rb'
-
-describe "Request" do
-
-  it "should be valid if starts with a request line, is followed by CRLF" do
-    Request.new("GET http://edendevelopment.es HTTP/1.1|u000D\u000A\u000D\u000A").should be_valid
-  end
-
-end
